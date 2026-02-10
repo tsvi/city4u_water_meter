4993a12873f348949e4bd2069515e836783e2d6d
Tsvi Mostovicz <ttmost@gmail.com>
2026-02-10 22:05:02 +0200

    Simplify release script - let bump-my-version handle dry-run
    - Remove conditional logic checking for -n/--dry-run flags
    - Pass all arguments directly to bump-my-version
    - Git push does nothing if bump-my-version didn't modify files (dry-run mode)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-# Release script that bumps version and optionally pushes to git
+# Release script that bumps version and pushes to git
+# Note: If you use -n or --dry-run, bump-my-version won't modify files,
+# so git push will just report "everything up-to-date"
 
-# Check if dry-run flag is present
-case "$*" in
-  *-n*|*--dry-run*)
-    # Dry-run mode: preview changes only, don't modify files or push
-    # The -n or --dry-run flag tells bump-my-version to show what would change without modifying any files
-    bump-my-version bump "$@"
-    ;;
-  *)
-    # Normal mode: bump version, commit, tag, and push
-    bump-my-version bump "$@" && git push && git push --tags
-    ;;
-esac
+bump-my-version bump "$@" && git push && git push --tags
