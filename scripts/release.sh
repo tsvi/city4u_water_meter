a2a9e15d8e7e2dd697f6866dd50e114c5fcea7d2
Tsvi Mostovicz <ttmost@gmail.com>
2026-02-10 22:07:15 +0200

    Move release command inline to pyproject.toml
    - Remove scripts/release.sh file
    - Put one-liner directly in pdm scripts configuration
    - Simpler and keeps all configuration in one place

--- scripts/release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Release script that bumps version and pushes to git
-# Note: If you use -n or --dry-run, bump-my-version won't modify files,
-# so git push will just report "everything up-to-date"
-
-bump-my-version bump "$@" && git push && git push --tags
